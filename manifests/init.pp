a79979039ee96c31a6b64861ea04720816ca4f54
Thor77 <thor77@thor77.org>
2020-05-18 22:10:12 +0200

    Use hiera for default parameters

--- manifests/init.pp
+++ manifests/init.pp
@@ -6,14 +6,14 @@
 # @param service_name [String] name of the chasquid service
 # @param config_path [String] path to chasquid config file
 class chasquid (
-  Hash $config = {},
-  Array $domains = [],
-  String $package_name = $chasquid::params::package_name,
-  String $package_version = $chasquid::params::package_version,
-  String $service_name = $chasquid::params::service_name,
-  String $config_directory = $chasquid::params::config_directory,
-  String $config_path = $chasquid::params::config_path,
-) inherits chasquid::params {
+  Hash $config,
+  Array $domains,
+  String $package_name,
+  String $package_version,
+  String $service_name,
+  String $config_directory,
+  String $config_path,
+) {
   contain chasquid::install
   contain chasquid::config
   contain chasquid::service
--- manifests/params.pp
+++
@@ -1,8 +0,0 @@
-# @summary Default parameters
-class chasquid::params {
-  $package_name = 'chasquid'
-  $package_version = 'installed'
-  $service_name = 'chasquid'
-  $config_directory = '/etc/chasquid'
-  $config_path = "${config_directory}/chasquid.conf"
-}
