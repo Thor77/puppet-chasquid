a8d4c98835d7780bd6698729f00734589b609bb4
Thor77 <thor77@thor77.org>
2020-05-27 22:34:13 +0200

    Add support for dkim domain options

--- manifests/setup.pp
+++ manifests/setup.pp
@@ -1,8 +1,17 @@
 # @summary Setup chasquid
 class chasquid::setup {
-  $chasquid::domains.each |$domain| {
-    file { "${chasquid::config_directory}/domains/${domain}":
+  $chasquid::domains.each |$domain, $params| {
+    $domain_directory = "${chasquid::config_directory}/domains/${domain}"
+    file { $domain_directory:
       ensure => 'directory',
+    }
+    if $params['dkim_key'] and $params['dkim_selector'] {
+      file { "${domain_directory}/dkim_privkey.pem":
+        content => $params['dkim_key'],
+      }
+      file { "${domain_directory}/dkim_selector":
+        content => $params['dkim_selector'],
+      }
     }
   }
 }
